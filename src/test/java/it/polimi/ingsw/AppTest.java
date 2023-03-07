f81a06a285465738fd08fbb8b63bc3b4b12f3aba
federico <federico.deintrona@mail.polimi.it>
2023-03-07 14:51:51 +0100

    Struttura base del progetto

---
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -0,0 +1,57 @@
+package it.polimi.ingsw;
+
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+
+
+class AppTest {
+    @Test
+    void theAnswer() {
+        assertEquals(42, 22 + 20);
+    }
+}
+
+
+
+
+/*
+public class AppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+/*
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+/*
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+/*
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}*/
