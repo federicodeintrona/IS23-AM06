cec5099463d70c1a509160c5ba16272e6361dca9
Antonio Di Paola <antonio1.dipaola@mail.polimi.it>
2023-03-15 15:55:44 +0100

    Sachet progress

--- src/main/java/it/polimi/ingsw/Sachet.java
+++ src/main/java/it/polimi/ingsw/Sachet.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw;
 
+import java.util.ArrayList;
+
 public class Sachet {
+    private ArrayList<Tiles> sachet;
+
+    public void draw(){
+
+    }
+    public int remainingTiles(){
+        int remTiles;
+        remTiles=0;
+        return remTiles;
+    }
+    public int remainingTilesPerColor(Tiles tiles){
+        int remTiles;
+        remTiles=0;
+        return remTiles;
+    }
 }
