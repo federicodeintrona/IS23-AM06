197dbd2d19f5434020c643107297f87d6950a5c6
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2023-03-16 21:17:13 +0100

    creation PersonalObjective's subclasses

--- src/main/java/it/polimi/ingsw/PersonalObjective/PersonalObjective.java
+++ src/main/java/it/polimi/ingsw/PersonalObjective/PersonalObjective.java
@@ -1,9 +1,15 @@
 package it.polimi.ingsw.PersonalObjective;
 
+import it.polimi.ingsw.Player;
+
 import java.util.ArrayList;
 
 public abstract class PersonalObjective {
 
     protected static ArrayList<Class> subclasses = new ArrayList();
 
+//metodi
+        //ritorna il numero di obiettivi completati -->
+        // --> posizione-colore carta PersonalObjective coincide con posizione-colore nella Bookshelf
+        public abstract int checkCondition(Player player);
 }
