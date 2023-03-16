d0c7e1949bb6ee6e59699b5ec1aa80a7d7b7be7d
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2023-03-16 21:20:34 +0100

    creation PersonalObjective's subclasses

--- src/main/java/it/polimi/ingsw/PersonalObjective/PersonalObjective.java
+++ src/main/java/it/polimi/ingsw/PersonalObjective/PersonalObjective.java
@@ -2,13 +2,67 @@ package it.polimi.ingsw.PersonalObjective;
 
 import it.polimi.ingsw.Player;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public abstract class PersonalObjective {
 
     protected static ArrayList<Class> subclasses = new ArrayList();
 
-//metodi
+
+    static{
+        try {
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective1");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective2");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective3");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective4");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective5");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective6");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective7");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective8");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective9");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective10");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective11");
+            Class.forName("it.polimi.ingsw.PersonalObjective.PersonalObjective12");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static ArrayList<PersonalObjective> randomSubclass(int num) throws InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+
+
+        ArrayList temp = (ArrayList) subclasses.clone();
+        Random rand = new Random();
+
+        System.out.println(subclasses.size());
+        System.out.println(temp.size());
+
+
+        ArrayList<PersonalObjective> result = new ArrayList<PersonalObjective>();
+
+        /*for (int i = 0; i< temp.size();i++){
+            System.out.println(temp.get(i));
+        }*/
+
+        for(int i = 0; i<num; i++ ){
+            int index = rand.nextInt(subclasses.size());
+            Constructor c= subclasses.get(index).getDeclaredConstructor();
+            temp.remove(index);
+            result.add((PersonalObjective) c.newInstance() );
+        }
+
+
+        return result;
+
+    }
+
+
+    //metodi
         //ritorna il numero di obiettivi completati -->
         // --> posizione-colore carta PersonalObjective coincide con posizione-colore nella Bookshelf
         public abstract int checkCondition(Player player);
