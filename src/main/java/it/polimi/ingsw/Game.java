ed1ea86be75976dee80349a970152eb1d513d68e
Frost40 <126003581+Frost40@users.noreply.github.com>
2023-03-16 18:08:01 +0100

    update Game

--- src/main/java/it/polimi/ingsw/Game.java
+++ src/main/java/it/polimi/ingsw/Game.java
@@ -1,9 +1,22 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.CommonObjective.CommonObjective;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Game {
     int turns = 0;
+    int numOfPlayers = 0;
+    int poitntsCommonObj1 = 8;
+    int poitntsCommonObj2 = 8;
+    Set<Player> players = new HashSet<>();
+    Set<CommonObjective> commonObjectives= new HashSet<>();
+    Board board;
 
+    // method to update the turns counter
     public void updateTurn(){
         turns++;
     }
+
 }
