046de0e70b7ee9a6d35799dd998da8f29063cc03
Antonio Di Paola <antonio1.dipaola@mail.polimi.it>
2023-03-16 11:39:41 +0100

    BookShelf progress

--- src/main/java/it/polimi/ingsw/Sachet.java
+++ src/main/java/it/polimi/ingsw/Sachet.java
@@ -5,6 +5,28 @@ import java.util.ArrayList;
 public class Sachet {
     private ArrayList<Tiles> sachet;
 
+    public Sachet() {
+        sachet=new ArrayList<Tiles>(132);
+        for (int i=0;i<22;i++){
+            sachet.add(Tiles.GREEN);
+        }
+        for (int i=0;i<22;i++){
+            sachet.add(Tiles.YELLOW);
+        }
+        for (int i=0;i<22;i++){
+            sachet.add(Tiles.PINK );
+        }
+        for (int i=0;i<22;i++){
+            sachet.add(Tiles.WHITE);
+        }
+        for (int i=0;i<22;i++){
+            sachet.add(Tiles.BLUE);
+        }
+        for (int i=0;i<22;i++){
+            sachet.add(Tiles.LIGHT_BLUE);
+        }
+    }
+
     public void draw(){
 
     }
