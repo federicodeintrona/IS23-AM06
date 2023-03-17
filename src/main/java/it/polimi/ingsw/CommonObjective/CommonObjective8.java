8543cde1e88c57942b13d2d548beb586b1a479d4
frost40 <flavio.dilorenzo@mail.polimi.it>
2023-03-17 19:02:10 +0100

    CommonObjective8 completed

--- src/main/java/it/polimi/ingsw/CommonObjective/CommonObjective8.java
+++ src/main/java/it/polimi/ingsw/CommonObjective/CommonObjective8.java
@@ -1,7 +1,36 @@
 package it.polimi.ingsw.CommonObjective;
 
+import it.polimi.ingsw.Player;
+import it.polimi.ingsw.Tiles;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class CommonObjective8 extends CommonObjective{
     static{
         subclasses.add(CommonObjective8.class);
+    }
+
+    public boolean checkCondition(Player player){
+        int count = 0;
+        int numOfColor;
+        int i = 0;
+        Set<Tiles> buffer = new HashSet<>();
+
+        while ((count < 4) && (i < 6)){
+            buffer.add(player.getBookshelf().getTiles().getTile(i, 0));
+            numOfColor = 1;
+            for (int j=1; j<5; j++){
+                if (!buffer.contains(player.getBookshelf().getTiles().getTile(i, j))){
+                    buffer.add(player.getBookshelf().getTiles().getTile(i, j));
+                    numOfColor++;
+                }
+            }
+            if (numOfColor < 4) count++;
+            buffer.clear();
+            i++;
+        }
+        if (count == 4) return true;
+        else return false;
     }
 }
