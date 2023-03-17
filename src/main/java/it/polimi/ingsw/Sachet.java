3d0ff466cbcf278834c84e7dc59156ce9875feaf
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2023-03-17 10:31:54 +0100

    modified Sachet class

--- src/main/java/it/polimi/ingsw/Sachet.java
+++ src/main/java/it/polimi/ingsw/Sachet.java
@@ -1,34 +1,50 @@
 package it.polimi.ingsw;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Sachet {
-    private ArrayList<Tiles> sachet;
+//attributi
+    private static ArrayList<Tiles> sachet=new ArrayList<Tiles>();
 
+    //creazione sachet vero e proprio
+    // --> vero sacchetto contenente le tessere (contiene 132 tessere = 6tipi * 22tessere)
+    static{
+        //132tiles = 6tipi * 22tiles
+
+    }
+
+//-------------------------------------------------------------------------------------------------------\\
+
+//metodi
+    //getter sachet --> ritorna il set di Tiles
+    public static ArrayList<Tiles> getSachet() {
+        return sachet;
+    }
+
+
+
+    //1. scegliere una tiles --> posizione randomica
+    //2. rimuovere la tiles dal set --> removeTiles
     public Tiles draw(){
         return Tiles.YELLOW;
     }
+
+    //ritorna il numero di tessere rimanenti nel sachet - OK
     public int remainingTiles(){
-        int remTiles;
-        remTiles=0;
-        return remTiles;
+        return sachet.size();
     }
+
+    //ritorna il numero di tessere rimanenti nel sachet del colore tiles richiesto
     public int remainingTilesPerColor(Tiles tiles){
-        int remTiles;
-        remTiles=0;
-        return remTiles;
+        return 0;
     }
 
-    public void setSachet(ArrayList<Tiles> sachet) {
-        this.sachet = sachet;
-    }
-
-    //aggiunge tiles nel sachet --> aggiunta ad array list
+    //aggiunge tiles nel sachet --> aggiunta ad array list - OK
     public void addTiles(Tiles til){
         sachet.add(til);
     }
 
-    //rimuove tiles dal sachet in posizione i
+    //rimuove tiles dal sachet in posizione i - OK
     public void removeTiles(int i){
         sachet.remove(i);
     }
