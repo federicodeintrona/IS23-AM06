1365a13e8eb340b5ec438344b162301a505567aa
frost40 <flavio.dilorenzo@mail.polimi.it>
2023-03-18 20:59:00 +0100

    CommonObjective3 completed

--- src/main/java/it/polimi/ingsw/CommonObjective/CommonObjective3.java
+++ src/main/java/it/polimi/ingsw/CommonObjective/CommonObjective3.java
@@ -1,13 +1,86 @@
 package it.polimi.ingsw.CommonObjective;
 
+import it.polimi.ingsw.Matrix;
 import it.polimi.ingsw.Player;
+import it.polimi.ingsw.Tiles;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CommonObjective3 extends CommonObjective{
 
     static{
         subclasses.add(CommonObjective3.class);
     }
+
     public boolean checkCondition(Player player) {
+        int numOfGroups = 0;
+        boolean[][] visited = new boolean[6][5];
+
+        // Loop through all the cells in the matrix
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (visited[i][j] || player.getBookshelf().getTiles().getTile(i, j).equals(Tiles.EMPTY)) {
+
+                    // Skip if cell already visited or set to EMPTY
+                    continue;
+                }
+
+                // Performing BFS (algoritmo di ricerca in ampiezza) on this node
+                Queue<Point> queue = new LinkedList<>();
+                queue.add(new Point(i, j));
+                visited[i][j] = true;
+                int count = 1;
+
+                while (!queue.isEmpty()) {
+                    Point current = queue.poll();
+
+                    // Check all adjacent cells with the same color
+                    for (Point adjacent : getAdjacentSameColor(player.getBookshelf().getTiles(), current)) {
+                        int x = adjacent.x;
+                        int y = adjacent.y;
+                        if (!visited[x][y]) {
+                            visited[x][y] = true;
+                            queue.add(adjacent);
+                            count++;
+
+                            // Four adjacent cells of the same color found
+                            if (count == 4) numOfGroups++;
+
+                            // Four groups that meet the criteria found
+                            if (numOfGroups == 4) return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        // No four adjacent cells of the same color found
         return false;
+    }
+
+    private static ArrayList<Point> getAdjacentSameColor(Matrix matrix, Point point) {
+        ArrayList<Point> adjacent = new ArrayList<>();
+        int x = point.x;
+        int y = point.y;
+        Tiles color = matrix.getTile(x, y);
+
+        // Checking adjacent cells with the same color
+        if (x > 0 && matrix.getTile(x - 1, y) == color) {
+            adjacent.add(new Point(x - 1, y));
+        }
+        if (x < 5 && matrix.getTile(x + 1, y) == color) {
+            adjacent.add(new Point(x + 1, y));
+        }
+        if (y > 0 && matrix.getTile(x, y - 1) == color) {
+            adjacent.add(new Point(x, y - 1));
+        }
+        if (y < 4 && matrix.getTile(x, y + 1) == color) {
+            adjacent.add(new Point(x, y + 1));
+        }
+
+        return adjacent;
     }
 }
