b7121905728cc4345e8ecd839d22e2594d65704d
federicodeintrona <federico.deintrona@mail.polimi.it>
2023-03-23 11:36:16 +0100

    Matrix Test update

--- src/test/java/it/polimi/ingsw/MatrixTest.java
+++ src/test/java/it/polimi/ingsw/MatrixTest.java
@@ -10,23 +10,28 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixTest {
+        public Matrix m;
 
+        @BeforeEach
+        void setUp(){
+             m = new Matrix(3,3);
+            for(int i=0;i<m.getNumCols();i++) {
+                m.setTile(Tiles.GREEN, 0,i);
+                m.setTile(Tiles.BLUE, 1,i);
+                m.setTile(Tiles.YELLOW, 2,i);
+            }
+        }
 
 
 
     @Test
     void GetaandSetCoordTile() {
-        Matrix m = new Matrix(3,3);
-        for(int i=0;i<m.getNumCols();i++) {
-            m.setTile(Tiles.GREEN, 0,i);
-            m.setTile(Tiles.BLUE, 1,i);
-            m.setTile(Tiles.YELLOW, 2,i);
-        }
+
 
         assertThrows(IndexOutOfBoundsException.class,()->m.getTile(2,4));
         assertEquals(m.getTile(0,0), Tiles.GREEN,"0,0 expected Green");
         assertEquals(m.getTile(1,2), Tiles.BLUE,"1,2 expected Blue");
-        assertEquals(m.getTile(1,0), Tiles.BLUE,"1,2 expected Blue");
+        assertEquals(m.getTile(1,0), Tiles.BLUE,"1,0 expected Blue");
         assertEquals(m.getTile(2,1), Tiles.YELLOW,"2,1 expected Yellow");
     }
 
@@ -34,7 +39,6 @@ class MatrixTest {
 
     @Test
     void GetandSetPointsTile() {
-        Matrix m = new Matrix(3,3);
 
         for(int i=0;i<m.getNumCols();i++) {
             Point p0 = new Point(0,i);
@@ -47,7 +51,7 @@ class MatrixTest {
 
         assertEquals(m.getTile(0,0), Tiles.GREEN,"0,0 expected Green");
         assertEquals(m.getTile(1,2), Tiles.BLUE,"1,2 expected Blue");
-        assertEquals(m.getTile(1,0), Tiles.BLUE,"1,2 expected Blue");
+        assertEquals(m.getTile(1,0), Tiles.BLUE,"1,0 expected Blue");
         assertEquals(m.getTile(2,1), Tiles.YELLOW,"2,1 expected Yellow");
 
     }
@@ -55,12 +59,6 @@ class MatrixTest {
 
     @Test
     void getColumn() {
-        Matrix m = new Matrix(3,3);
-        for(int i=0;i<m.getNumCols();i++) {
-            m.setTile(Tiles.GREEN, 0,i);
-            m.setTile(Tiles.BLUE, 1,i);
-            m.setTile(Tiles.YELLOW, 2,i);
-        }
         Tiles[] t = {Tiles.GREEN,Tiles.BLUE,Tiles.YELLOW};
         assertArrayEquals(t,m.getColumn(1).toArray());
 
@@ -68,31 +66,40 @@ class MatrixTest {
 
 
 
-
-
-/*
-
-
     @Test
     void remove() {
-    }
 
-    @Test
-    void testRemove() {
-    }
+        Point p = new Point(1,2);
+        m.remove(2,2);
+        m.remove(p);
+        assertEquals(Tiles.EMPTY,m.getTile(2,2));
+        assertEquals(Tiles.EMPTY,m.getTile(p));
+        assertEquals(Tiles.GREEN,m.getTile(0,1));
 
-
-    @Test
-    void testGetTile() {
     }
 
 
 
     @Test
     void columnIsFull() {
+       m.remove(2,1);
+
+       assertEquals(true,m.columnIsFull(2));
+       assertEquals(false,m.columnIsFull(1));
+
+
+
     }
 
     @Test
     void rowIsFull() {
-    }*/
+        m.remove(0,0);
+
+        assertEquals(true,m.rowIsFull(2));
+        assertEquals(false,m.rowIsFull(0));
+
+
+
+
+    }
 }
