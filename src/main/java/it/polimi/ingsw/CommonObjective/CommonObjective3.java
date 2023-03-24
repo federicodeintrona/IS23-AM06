d5ef0becdb7e88d94e8cd635f9bcdde848c7ad13
frost40 <flavio.dilorenzo@mail.polimi.it>
2023-03-24 20:25:33 +0100

    solved problem in CommonObjective3

--- src/main/java/it/polimi/ingsw/CommonObjective/CommonObjective3.java
+++ src/main/java/it/polimi/ingsw/CommonObjective/CommonObjective3.java
@@ -46,12 +46,16 @@ public class CommonObjective3 extends CommonObjective{
                             count++;
 
                             // Four adjacent cells of the same color found
-                            if (count == 4) numOfGroups++;
-
-                            // Four groups that meet the criteria found
-                            if (numOfGroups == 4) return true;
+                            if (count == 4) {
+                                numOfGroups++;
+                                queue.clear();
+                                break;
+                            }
                         }
                     }
+
+                    // Four groups that meet the criteria found
+                    if (numOfGroups == 4) return true;
                 }
             }
         }
