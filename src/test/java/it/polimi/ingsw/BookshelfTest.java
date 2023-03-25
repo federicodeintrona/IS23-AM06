996b826a3277a1928efba5dfb1061ed26033336a
Antonio Di Paola <antonio1.dipaola@mail.polimi.it>
2023-03-25 11:26:13 +0100

    Bookshelf Test in my branch

---
+++ src/test/java/it/polimi/ingsw/BookshelfTest.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BookshelfTest {
+    public Bookshelf b;
+
+    @BeforeEach
+    void setUp(){
+        b=new Bookshelf();
+    }
+    @Test
+    void CheckConstructor(){
+
+    }
+}
