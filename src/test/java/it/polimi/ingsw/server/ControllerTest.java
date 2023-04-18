11d391aa52e9ad9671ed810f033f9a07d2b052cb
federicodeintrona <federico.deintrona@mail.polimi.it>
2023-04-18 14:39:55 +0200

    .

--- src/test/java/it/polimi/ingsw/server/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/server/ControllerTest.java
@@ -13,7 +13,7 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
-/*
+
     public HashMap<Integer,Model> games = new HashMap<>();
     public HashMap<String,ServerClientHandler> clients = new HashMap<>();
     public Controller controller = new Controller(games,clients);
@@ -25,52 +25,6 @@ class ControllerTest {
     @BeforeEach
     void setUp(){
 
-        ServerClientHandler client0 = new ServerClientHandler(controller);
-        client0.setNickname("player0");
-        client0.setGameID(0);
-        client0.setPlayer(new Player(client0.getNickname()));
-        clients.put(client0.getNickname(),client0);
-        players0.add(client0.getPlayer());
-        views0.add(new CLIView());
-
-        ServerClientHandler client1 = new ServerClientHandler(controller);
-        client1.setNickname("player1");
-        client1.setGameID(0);
-        client1.setPlayer(new Player(client1.getNickname()));
-        clients.put(client1.getNickname(),client1);
-        players0.add(client1.getPlayer());
-        views0.add(new CLIView());
-
-        ServerClientHandler client2 = new ServerClientHandler(controller);
-        client2.setNickname("player2");
-        client2.setGameID(1);
-        client2.setPlayer(new Player(client2.getNickname()));
-        clients.put(client2.getNickname(),client2);
-        players1.add(client2.getPlayer());
-        views1.add(new CLIView());
-
-
-        ServerClientHandler client3 = new ServerClientHandler(controller);
-        client3.setNickname("player3");
-        client3.setGameID(1);
-        client3.setPlayer(new Player(client3.getNickname()));
-        clients.put(client3.getNickname(),client3);
-        players1.add(client3.getPlayer());
-        views1.add(new CLIView());
-
-        ServerClientHandler client4 = new ServerClientHandler(controller);
-        client4.setNickname("player4");
-        client4.setGameID(1);
-        client4.setPlayer(new Player(client4.getNickname()));
-        clients.put(client4.getNickname(),client4);
-        players1.add(client4.getPlayer());
-        views1.add(new CLIView());
-
-        games.put(0,new Model(players0,views0));
-        games.put(1,new Model(players1,views1));
-
-        controller.startGame(0);
-        controller.startGame(1);
     }
 
     @Test
@@ -124,7 +78,7 @@ class ControllerTest {
         Random rdm = new Random();
         return new Point(rdm.nextInt(6),rdm.nextInt(5));
 
-    }*/
+    }
 }
 
 
