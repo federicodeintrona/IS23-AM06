8e4c010f72697c3cb2afc3bba23be5cd1b71996e
federicodeintrona <federico.deintrona@mail.polimi.it>
2023-05-04 19:13:39 +0200

    ControllerTest

--- src/test/java/it/polimi/ingsw/server/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/server/ControllerTest.java
@@ -27,18 +27,15 @@ class ControllerTest {
     public ArrayList<ArrayList<Player>> players = new ArrayList<>();
     public ArrayList<ArrayList<VirtualView>> views = new ArrayList<>();
 
-    public int gameNumber=2;
-    public int playerNumber=3;
+    private static final int gameNumber=2;
+    private static final int  playerNumber=3;
+
     @BeforeEach
     void setUp() {
 
-        modelmap.put(0,new Model());
-        modelmap.put(1,new Model());
 
-        //j = number of games
         for(int j = 0; j<gameNumber; j++) {
-
-            //Add a game
+            modelmap.put(j,new Model());
             players.add(new ArrayList<>());
             views.add(new ArrayList<>());
 
@@ -73,118 +70,12 @@ class ControllerTest {
 
     @Test
     void removeTiles() {
-        ArrayList<Point> points = new ArrayList<>();
-        Message msg;
 
-
-        //OutOfDomain
-        points.add(new Point(45,20));
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
-        msg = controller.removeTiles(0,modelmap.get(0).getCurrPlayer().getUsername(),points);
-        assertEquals("You selected a point outside the board",msg.getUsername());
-        points.removeAll(points);
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
-
-        //TilesCannotBeSelected
-        points.add(new Point(45,20));
-        msg = controller.removeTiles(0,modelmap.get(0).getCurrPlayer().getUsername(),points);
-        assertEquals("One of the tiles cannot be selected",msg.getUsername());
-        points.removeAll(points);
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
-
-        //TilesNotAdjacent
-        points.add(new Point(45,20));
-        msg = controller.removeTiles(0,modelmap.get(0).getCurrPlayer().getUsername(),points);
-        assertEquals("The tiles are not adjacent to each other",msg.getUsername());
-        points.removeAll(points);
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
-
-        //IllegalArgumentException
-        msg = controller.removeTiles(0,modelmap.get(0).getCurrPlayer().getUsername(),points);
-        assertEquals("You selected 0 tiles",msg.getUsername());
-        points.removeAll(points);
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
-
-        //TooManySelected
-        points.add(new Point(45,20));
-        msg = controller.removeTiles(0,modelmap.get(0).getCurrPlayer().getUsername(),points);
-        assertEquals("You selected too many tiles",msg.getUsername());
-        points.removeAll(points);
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
-
-        //NotCurrentPlayer
-        points.add(new Point(3,5));
-        msg = controller.removeTiles(0,modelmap.get(0).getNextPlayer().getUsername(),points);
-        assertEquals("It's not your turn",msg.getUsername());
-        points.removeAll(points);
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
-
-        //MoveNotPossible
-        points.add(new Point(45,20));
-        msg = controller.removeTiles(0,modelmap.get(0).getCurrPlayer().getUsername(),points);
-        assertEquals("You can't do that now",msg.getUsername());
-        points.removeAll(points);
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
-
-
-        //Normal
-        points.add(new Point(45,20));
-        msg = controller.removeTiles(0,modelmap.get(0).getCurrPlayer().getUsername(),points);
-        assertEquals("Move successful",msg.getUsername());
-        points.removeAll(points);
-        modelmap.get(0).setState(GameState.CHOOSING_TILES);
     }
 
     @Test
     void swapOrder() {
-        ArrayList<Tiles> selected = new ArrayList<>();
-        selected.add(Tiles.GREEN);
-        selected.add(Tiles.BLUE);
-        selected.add(Tiles.YELLOW);
-        modelmap.get(0).setSelectedTiles(selected);
-        Message msg;
 
-        ArrayList<Integer> order = new ArrayList<>();
-
-        //NotCurrentPlayer
-        order.add(0);
-        order.add(1);
-        order.add(2);
-        msg = controller.swapOrder(order,0,modelmap.get(0).getNextPlayer().getUsername());
-        assertEquals("You are not the current player",msg.getUsername());
-        order.removeAll(order);
-
-        //IllegalArgument
-        order.add(0);
-        order.add(23);
-        order.add(2);
-        msg = controller.swapOrder(order,0,modelmap.get(0).getCurrPlayer().getUsername());
-        assertEquals("You cannot choose these positions",msg.getUsername());
-        order.removeAll(order);
-
-        //TooManySelected
-        order.add(0);
-        order.add(1);
-        order.add(2);
-        order.add(3);
-        msg = controller.swapOrder(order,0,modelmap.get(0).getCurrPlayer().getUsername());
-        assertEquals("Incorrect number of orders",msg.getUsername());
-        order.removeAll(order);
-
-        //MoveNotPossible
-        order.add(0);
-        order.add(1);
-        order.add(2);
-        msg = controller.swapOrder(order,0,modelmap.get(0).getCurrPlayer().getUsername());
-        assertEquals("You can't do that now",msg.getUsername());
-        order.removeAll(order);
-
-        //Normal
-        order.add(0);
-        order.add(2);
-        order.add(1);
-        msg = controller.swapOrder(order,0,modelmap.get(0).getCurrPlayer().getUsername());
-        assertEquals("Move successful",msg.getUsername());
 
     }
 
