3bdb4f81270d5eec885db7da733b9799ec5cfcb4
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2023-05-07 20:28:56 +0200

    merging cli into develop

---
+++ src/main/java/it/polimi/ingsw/client/Messages/MatrixMessage.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.client.Messages;
+
+import it.polimi.ingsw.utils.Matrix;
+
+public class MatrixMessage extends Message {
+
+    private Matrix matrix;
+
+    public Matrix getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(Matrix matrix) {
+        this.matrix = matrix;
+    }
+}
