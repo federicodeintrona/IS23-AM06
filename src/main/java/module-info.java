b16be757a34555f5f4f22a63a74a1310a68533c3
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2023-05-17 11:27:36 +0200

    gui: add javafx to develope_2.0

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,9 +8,13 @@ module it.polimi.ingsw {
     requires java.desktop;
     requires openpojo;
     requires javatuples;
+    requires java.sql;
 
     opens it.polimi.ingsw.app to javafx.fxml, javafx.controls, javafx.graphics;
     exports it.polimi.ingsw.app;
 
     opens it.polimi.ingsw.server.CommonObjective to openpojo;
+    opens it.polimi.ingsw.client to java.rmi;
+    opens it.polimi.ingsw.utils.Messages to java.rmi;
+    opens it.polimi.ingsw.server to java.rmi;
 }
