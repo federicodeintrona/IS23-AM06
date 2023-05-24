a512ea7bf4fdd5c7c8ba71a4f3ee01b60faa63ac
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2023-05-24 19:04:57 +0200

    gui_merge2.1: ended 2 players' game fxml

--- src/main/java/it/polimi/ingsw/client/View/GUI/Scene/GameController.java
+++ src/main/java/it/polimi/ingsw/client/View/GUI/Scene/GameController.java
@@ -8,10 +8,12 @@ import it.polimi.ingsw.utils.Matrix;
 import it.polimi.ingsw.utils.Tiles;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
@@ -22,25 +24,29 @@ public class GameController implements Initializable {
     private ClientState clientState;
     @FXML
     private GridPane boardGrid;
+    @FXML
+    private GridPane commonGrid;
+    @FXML
+    private GridPane myBookshelfGrid;
+    @FXML
+    private Label otherPlayerLabel;
+    @FXML
+    private GridPane otherPlayerBookshelfGrid;
+    @FXML
+    private ImageView personalObjectiveImageView;
+    @FXML
+    private Label myPointsLabel;
+    @FXML
+    private Label otherPlayerPointsLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         clientState = guiController.getState();
         initializeBoardGrid();
+        initializeCommonGrid();
     }
 
-    //la metrice è 11x11 ==> getTile -1
-    public void initializeBoardGrid(){
-        Matrix matrix=clientState.getBoard();
-        for (int i = 1; i < Define.NUMBEROFROWS_BOARD.getI()+1; i++) {
-            for (int j = 1; j < Define.NUMBEROFCOLUMNS_BOARD.getI()+1; j++) {
-                if (!matrix.getTile(i-1,j-1).equals(Tiles.NOT_ALLOWED) && !matrix.getTile(i-1,j-1).equals(Tiles.EMPTY)) {
-                    boardGrid.add(setTiles(matrix.getTile(i-1, j-1)), j, i); //lavora colonna - riga
-                }
-            }
-        }
-    }
-
+    //setta le tessere - dal colore all'immagine
     private ImageView setTiles(Tiles tile){
 
 //
@@ -58,4 +64,43 @@ public class GameController implements Initializable {
         imageView.setFitWidth(50);
         return imageView;
     }
+
+
+    //la metrice è 11x11 ==> getTile -1
+    public void initializeBoardGrid(){
+        Matrix matrix=clientState.getBoard();
+        for (int i = 1; i < Define.NUMBEROFROWS_BOARD.getI()+1; i++) {
+            for (int j = 1; j < Define.NUMBEROFCOLUMNS_BOARD.getI()+1; j++) {
+                if (!matrix.getTile(i-1,j-1).equals(Tiles.NOT_ALLOWED) && !matrix.getTile(i-1,j-1).equals(Tiles.EMPTY)) {
+                    boardGrid.add(setTiles(matrix.getTile(i-1, j-1)), j, i); //lavora colonna - riga
+                }
+            }
+        }
+    }
+
+    //TODO inizializza i common objective
+    public void initializeCommonGrid(){
+
+    }
+
+    //TODO inizializza il nome dell'altro giocatore
+    public void initializeotherPlayerLabel(){
+    }
+
+    //TODO inizializza il personal objective
+    public void initializePersonalObjectiveImageView(){}
+
+    //TODO inizializza i tuoi punti
+    public void initializeMyPointsLabel(){
+        //my points are: ...
+    }
+
+    //TODO inizializza i punti dell'altro giocatore
+    public void initializeOtherPlayerPointsLabel(){}
+
+    //TODO update board, mybookshelf, otherplayerbookshelf, mypoints, otherplayerpoints
+
+    //TODO click on boardGrid
+
+
 }
