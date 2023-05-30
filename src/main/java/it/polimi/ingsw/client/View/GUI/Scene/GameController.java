c32c63b68681742c50dee5c1e7d7f062690f964d
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2023-05-30 15:32:33 +0200

    gui_merge2.1: update tiles' switch

--- src/main/java/it/polimi/ingsw/client/View/GUI/Scene/GameController.java
+++ src/main/java/it/polimi/ingsw/client/View/GUI/Scene/GameController.java
@@ -327,6 +327,7 @@ public class GameController implements Initializable, PropertyChangeListener,Sce
         if (clientState.getCurrentPlayer().equals(clientState.getMyUsername())) {
             selectedTilesDialog.setVisible(true);
             selectedTilesDialog.setDisable(false);
+            selectedTilesDialog.setStyle("-fx-background-color: null");
             //rendi visibile il bottone per finire lo switch e andare nella selezione della colonna
             endSwitch.setVisible(true);
             endSwitch.setDisable(false);
@@ -337,6 +338,9 @@ public class GameController implements Initializable, PropertyChangeListener,Sce
             selectedTiles2.setVisible(true);
             selectedTiles3.setDisable(false);
             selectedTiles3.setVisible(true);
+            //rendi invisibile la board
+            boardGrid.setDisable(true);
+            boardGrid.setVisible(false);
             switch (clientState.getSelectedTiles().size()) {
                 case 1 -> {
                     selectedTiles1.setImage(new Image(clientState.getSelectedTiles().get(0).getImage()[0]));
@@ -461,6 +465,10 @@ public class GameController implements Initializable, PropertyChangeListener,Sce
     private void rollbackClick(ActionEvent actionEvent){
         removeTiles=new ArrayList<>();
         boardGrid.getChildren().forEach(node -> node.setStyle("-fx-opacity: 1"));
+        rollbackButton.setVisible(false);
+        rollbackButton.setDisable(true);
+        confirmationButton.setVisible(false);
+        confirmationButton.setDisable(true);
     }
 
     //conferma l'ordine dello switch
@@ -523,6 +531,7 @@ public class GameController implements Initializable, PropertyChangeListener,Sce
         column5.setVisible(true);
         column5.setDisable(false);
 
+        boardGrid.setVisible(true);
         state = State.ADD;
     }
 
