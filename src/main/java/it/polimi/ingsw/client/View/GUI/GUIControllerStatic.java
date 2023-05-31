83b62eb58058012c84b95f6fb43ebc98cf7ccf9d
AlessandroConti11 <alessandro11.conti@mail.polimi.it>
2023-05-31 11:35:41 +0200

    develop: commenti

--- src/main/java/it/polimi/ingsw/client/View/GUI/GUIControllerStatic.java
+++ src/main/java/it/polimi/ingsw/client/View/GUI/GUIControllerStatic.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.client.View.GUI;
 
+//serve per fare in modo che i controller di scena abbiano il riferimento al GUIController
+//in modo che riescano a inviare messaggi al server
+//e a ricevere messaggi da server
 public class GUIControllerStatic {
     private static GUIController guiController;
     public static GUIController getGuiController() {
