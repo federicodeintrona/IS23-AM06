22193f90ea8367bb9eed4ce39dde32269b58ef19
Antonio Di Paola <antonio1.dipaola@mail.polimi.it>
2023-06-13 15:17:39 +0200

    javaDoc_NetworkerTcp: added javaDoc to NetworkerTcp

--- src/main/java/it/polimi/ingsw/client/NetworkerTcp.java
+++ src/main/java/it/polimi/ingsw/client/NetworkerTcp.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.client;
 
 
-import it.polimi.ingsw.client.View.CLI.CLIMain;
 import it.polimi.ingsw.client.View.View;
 import it.polimi.ingsw.utils.Messages.*;
 import it.polimi.ingsw.utils.JsonReader;
@@ -19,24 +18,25 @@ public class NetworkerTcp implements Networker, PropertyChangeListener {
     private Socket socket ;
     private ObjectOutputStream oos;
     private View view;
-    private final ClientState clientState;
+    private ClientState clientState;
 
     public NetworkerTcp(ClientState clientState,String host) {
         JsonReader config;
         try {
             InputStream is=this.getClass().getClassLoader().getResourceAsStream("ConnectionPorts.json");
             config=new JsonReader(is);
+            this.clientState = clientState;
+            NetworkerTcp.host =host;
+            port=config.getInt("tcpPort");
         } catch (IOException e) {
+            System.out.println("Input/Output problems. Try to close and reopen the program");
             e.printStackTrace();
-            throw new RuntimeException(e);
+            close();
         } catch (ParseException e) {
+            System.out.println("Problem with tha parsing of file json. Try to close and reopen the program");
             e.printStackTrace();
-            throw new RuntimeException(e);
+            close();
         }
-
-        this.clientState = clientState;
-        NetworkerTcp.host =host;
-        port=config.getInt("tcpPort");
     }
 
     public void initializeConnection() {
@@ -66,6 +66,10 @@ public class NetworkerTcp implements Networker, PropertyChangeListener {
 
     }
 
+    /**
+     * Sends to server the username selected by player
+     * @param username a message which contains the username selected by the palyer
+     */
     public void firstConnection (Message username){
         try {
             oos.writeObject(username);
@@ -75,6 +79,11 @@ public class NetworkerTcp implements Networker, PropertyChangeListener {
         }
 
     }
+
+    /**
+     * Sends to server the number of player selected
+     * @param numberOfPlayers a message which contains the number of player
+     */
     public void numberOfPlayersSelection(Message numberOfPlayers){
         try {
             oos.writeObject(numberOfPlayers);
@@ -85,6 +94,11 @@ public class NetworkerTcp implements Networker, PropertyChangeListener {
         }
 
     }
+
+    /**
+     * Sends to the server the positions of the tiles to remove from board
+     * @param tiles a message which contains the positions of the tiles to remove from board
+     */
     public void removeTilesFromBoard(Message tiles){
         try {
             oos.writeObject(tiles);
@@ -94,6 +108,11 @@ public class NetworkerTcp implements Networker, PropertyChangeListener {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Sends to the server the order of the tiles
+     * @param ints a message which contains the order of the tiles
+     */
     public void switchTilesOrder(Message ints){
         try {
             oos.writeObject(ints);
@@ -104,6 +123,10 @@ public class NetworkerTcp implements Networker, PropertyChangeListener {
         }
     }
 
+    /**
+     * Send to the server the column chosen from the player
+     * @param column a message which contains the column chosen by the player
+     */
     public void addTilesToBookshelf (Message column){
         try {
             oos.writeObject(column);
@@ -114,16 +137,29 @@ public class NetworkerTcp implements Networker, PropertyChangeListener {
         }
     }
 
+    /**
+     * Send to the view the message received
+     * @param evt A PropertyChangeEvent object describing the event source
+     *          and the property that has changed.
+     */
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         view.receivedMessage((Message) evt.getNewValue());
     }
 
+    /**
+     * Sets the view
+     * @param view The view crated by the client base
+     */
     @Override
     public void setView(View view) {
         this.view=view;
     }
 
+    /**
+     * Send to the server the chat message written by the player
+     * @param message a message which contains the chat message by the player
+     */
     @Override
     public void chat(Message message) {
         try {
