641b6074af3e09209d0aed95c1979acd0740ca56
Frost40 <126003581+Frost40@users.noreply.github.com>
2023-06-21 14:30:26 +0200

    NetworkerRmi javadoc

--- src/test/java/it/polimi/ingsw/utils/ChatControllerTest.java
+++ src/test/java/it/polimi/ingsw/utils/ChatControllerTest.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.utils;
 
+import it.polimi.ingsw.utils.Messages.ChatMessage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,10 +10,25 @@ class ChatControllerTest {
 
     @Test
     void getPublicChat() {
+        ChatController chatController = new ChatController();
+        Chat chat = new Chat();
+        ChatMessage message = new ChatMessage("obi-wan", "test");
+
+        chatController.getPublicChat().addMessage(message);
+        chat.addMessage(message);
+        Assertions.assertEquals(chat.getChatMessages().get(0), chatController.getPublicChat().getChatMessages().get(0));
     }
 
     @Test
     void getPrivateChat() {
+        ChatController chatController = new ChatController();
+        Chat chat = new Chat();
+        String receivingUsername = "anakin";
+        ChatMessage message = new ChatMessage("obi-wan", "test", receivingUsername);
+
+        chatController.getPrivateChat(receivingUsername).addMessage(message);
+        chat.addMessage(message);
+        Assertions.assertEquals(chat.getChatMessages().get(0), chatController.getPrivateChat(receivingUsername).getChatMessages().get(0));
     }
 
     @Test
