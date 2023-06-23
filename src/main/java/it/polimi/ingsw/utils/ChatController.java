4b82f1adc49c2029c168ce2ad29713469b265181
Frost40 <126003581+Frost40@users.noreply.github.com>
2023-06-23 22:23:24 +0200

    javadoc ChatController

--- src/main/java/it/polimi/ingsw/utils/ChatController.java
+++ src/main/java/it/polimi/ingsw/utils/ChatController.java
@@ -3,15 +3,56 @@ package it.polimi.ingsw.utils;
 import java.io.Serializable;
 import java.util.HashMap;
 
+/**
+ * <p>
+ *     Class to manage all Chats behavior
+ * </p>
+ * <p>
+ *     It contains:
+ *     <ul>
+ *         <li>1 publicChat</li>
+ *         <li>all privateChats grouped in a HashMap accessible by player's username</li>
+ *     </ul>
+ * </p>
+ * <p>
+ *     Its role is to group all chats in one place
+ *     and to manage their construction and calls
+ * </p>
+ */
 public class ChatController implements Serializable {
     private Chat publicChat;
     private HashMap<String, Chat> privateChats;
 
+    /**
+     * <p>
+     *     Constructor for Client
+     * </p>
+     * <p>
+     *     Instantiate both the publicChat and
+     *     the HashMap for the privateChats
+     * </p>
+     */
     public ChatController() {
         publicChat = new Chat();
         privateChats = new HashMap<>();
     }
 
+    /**
+     * <p>
+     *     Constructor for Server
+     * </p>
+     * <p>
+     *     Instantiate only the HashMap for the privateChats
+     * </p>
+     * <p>
+     *     In order to save memory the publicChat on server's
+     *     hand is separated from the ChatController because
+     *     equals for all players. In this way the maximum number
+     *     of Chats instanced will be, in a 4 players game, 13 (3
+     *     private chats for each player + 1 public instead of 4)
+     *     making the backup upload faster
+     * </p>
+     */
     public ChatController (boolean forServer){
         privateChats = new HashMap<>();
     }
